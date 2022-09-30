e66b51fd7629d38331fdf62f2bd06603ce1f62b5
dhfgy <113974987+dhfgy@users.noreply.github.com>
2022-09-30 11:07:39 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{														
+   	int n,sum=0,count;
+   	cout<<"enter a nunber";
+   	cin>>n;
+    for(int i=1;i < n+1;i++)
+    
+	return 0;
+ } 		
